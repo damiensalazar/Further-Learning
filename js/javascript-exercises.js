c87c6b0214f77d25ca50cb970240d92723f6bd56
Damien Salazar <salazar.damien.01@gmail.com>
2020-08-26 16:20:26 -0500

    More completed challenges

--- js/javascript-exercises.js
+++ js/javascript-exercises.js
@@ -1,3 +1,4 @@
+//say hello, say bye
 function sayHelloBye(name, num) {
     if (num == 1){
 
@@ -6,13 +7,103 @@ function sayHelloBye(name, num) {
         return "Bye " + name.charAt(0).toUpperCase() + name.slice(1)
     }
 }
-
+// //Given two integers, a and b, return true if a can be divided evenly by b.
 function dividesEvenly(a, b) {
     return a % b === 0
 }
-
+//correct the syntax
 function squared(b) {
     var a = b;
     return a * a
 }
+//Return the Next Number from the Integer Passed
+function addition(num) {
+    return num++ + 1
+}
 
+//Get absolute sum
+function getAbsSum(arr) {
+    var sum = 0
+    for (var i = 0; i <arr.length; i++) {
+        sum += Math.abs(arr[i]);
+    }
+    return sum
+
+}
+
+//convert hours into seconds
+function howManySeconds(hours) {
+    return hours*(60 * 60)
+
+
+}
+
+//football points
+function footballPoints(wins, draws, losses) {
+    return (wins * 3) + draws
+}
+
+//total amount of legs
+function animals(chickens, cows, pigs) {
+    return (chickens * 2) + (cows * 4) + (pigs * 4)
+}
+
+//get area of triangle
+
+function triArea(base, height) {
+    return (base * height) / 2
+}
+
+//return something
+function giveMeSomething(a) {
+    return "something" + " " + a
+}
+
+//profitable game
+function profitableGamble(prob, prize, pay) {
+    if (prob * prize > pay){
+        return true
+    }else{
+        return false
+    }
+}
+
+//equality check
+function checkEquality(a, b) {
+    if(a === b){
+        return true
+    }else{
+        return false
+    }
+
+}
+
+//less than 100
+function lessThan100(a, b) {
+    if(a + b < 100){
+        return true
+    }else{
+        return false
+    }
+}
+
+//find the index
+function search(arr, item) {
+    return arr.indexOf(item)
+}
+
+//compare strings by count of characters
+function comp(str1, str2) {
+    if(str1.length === str2.length){
+        return true
+    }else{
+        return false
+    }
+}
+
+//Return the Next Number from the Integer Passed
+function addition(num) {
+    return num++ + 1
+}
+
+//
